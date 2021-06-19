51a5af294792670c574b0af327b528822e6fcdb2
thedawnofallwars <thedawnofallwars@gmail.com>
2021-06-19 18:57:50 -0400

    update 1

--- uninstall.php
+++ uninstall.php
@@ -1,31 +1,15 @@
 <?php
-/**
- * Removes hooks which are instilled by mentions mod
- *
- * @author Shitiz Garg <mail@dragooon.net>
- * @copyright 2014 Shitiz Garg
- * @license Simplified BSD (2-Clause) License
- */
-
 if (!defined('SMF'))
 	require_once('SSI.php');
 
 $hooks = array(
-	'integrate_pre_include' => '$sourcedir/Mentions.php',
-	'integrate_profile_areas' => 'mentions_profile_areas',
-	'integrate_load_permissions' => 'mentions_permissions',
-	'integrate_bbc_codes' => 'mentions_bbc',
-	'integrate_menu_buttons' => 'mentions_menu',
-	'integrate_register' => 'mentions_register',
+	'integrate_pre_include' => '$sourcedir/References.php',
+	'integrate_profile_areas' => 'references_profile_areas',
+	'integrate_load_permissions' => 'references_permissions',
+	'integrate_bbc_codes' => 'references_bbc',
+	'integrate_menu_buttons' => 'references_menu',
+	'integrate_register' => 'references_register',
 );
 
 foreach ($hooks as $hook => $function)
 	remove_integration_function($hook, $function);
-
-$smcFunc['db_query']('', '
-	DELETE FROM {db_prefix}scheduled_tasks
-	WHERE task = {string:task}',
-	array(
-		'task' => 'removeMentions',
-	)
-);
