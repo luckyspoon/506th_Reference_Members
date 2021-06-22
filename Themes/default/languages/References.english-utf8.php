d7d8661ae5d61cb07c3ac2f14c9f96f94ddf4106
Owen Hardman <thedawnofallwars@gmail.com>
2021-06-22 12:28:16 +0000

    Tidy up

--- Themes/default/languages/References.english-utf8.php
+++ Themes/default/languages/References.english-utf8.php
@@ -2,8 +2,6 @@
 global $txt, $context;
 
 $txt['references'] = 'References';
-$txt['references_profile_title'] = 'Posts Referencing %s';
-$txt['references_post_subject'] = 'Subject';
 $txt['permissionname_reference_member'] = 'Reference members';
 $txt['permissionhelp_reference_member'] = 'Allow members to reference other members via $username syntax';
 $txt['references_permissions_notice'] = 'Please remember to set permissions to allow individual members to reference others';
